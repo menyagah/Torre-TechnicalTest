d5a29f129a6e3f1d5fd94292d540fc5d63fb106f
menyagah <menyagah27@gmail.com>
2021-11-22 01:59:29 +0300

    Separate function into it's own file

---
+++ src/debounce.js
@@ -0,0 +1,14 @@
+const debounce = (func, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+
+export default debounce;
