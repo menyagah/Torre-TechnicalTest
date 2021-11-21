65a0eaa2ee2d7bd2375c14a9a127a326a7c910fb
menyagah <menyagah27@gmail.com>
2021-11-21 14:33:05 +0300

    Add server port

--- server/server.js
+++ server/server.js
@@ -3,3 +3,8 @@ const cors = require('cors');
 const { default: axios } = require('axios');
 const app = express();
 app.use(cors())
+
+
+app.listen(3001, ()=> {
+    console.log('Listening on port 3001')
+});
