e76af7209d4f454341e2d3b38653692722fff141
menyagah <menyagah27@gmail.com>
2021-11-22 02:02:17 +0300

    Remove code from file, add imports and call functions

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import './style/styles.css';
+import trigger from './searchBios';
 
-const { default: axios } = require('axios');
 
-const fetchData = async (searchTerm) => {
-  // const response = await axios.get(`http://localhost:3001/people?username=${searchTerm}`); people route
-  const response = await axios.get(`http://localhost:3001/username?username=${searchTerm}`);
 
-  console.log(response.data);
-};
 
-const input = document.querySelector('input');
 
-const debounce = (func, delay = 500) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-};
-
-const onInput = (event) => {
-  fetchData(event.target.value);
-};
-
-input.addEventListener('input', debounce(onInput));
+trigger();
