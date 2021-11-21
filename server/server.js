f6354906df791eb20093fb162db059ec2ed915fb
menyagah <menyagah27@gmail.com>
2021-11-21 14:35:45 +0300

    Create a get user bio get route handler

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const { default: axios } = require('axios');
 const app = express();
 app.use(cors())
 
+app.get('/username', async (req,res)=> {
+    let endpoint = 'https://torre.bio/api/bios/' + req.query.username
+
+    const { data } = await axios.get(endpoint)
+    res.send(data || [])
+});
 
 app.listen(3001, ()=> {
     console.log('Listening on port 3001')
