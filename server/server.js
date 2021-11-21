8e8d042838f194a31b1a4b1ec9c95c2c270fa975
menyagah <menyagah27@gmail.com>
2021-11-21 15:16:24 +0300

    Install linter dependencies and fix linter errors

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const { default: axios } = require('axios');
+
 const app = express();
-app.use(cors())
+app.use(cors());
 
-app.get('/username', async (req,res)=> {
-    let endpoint = 'https://torre.bio/api/bios/' + req.query.username
+app.get('/username', async (req, res) => {
+  const endpoint = `https://torre.bio/api/bios/${req.query.username}`;
 
-    const { data } = await axios.get(endpoint)
-    res.send(data || [])
+  const { data } = await axios.get(endpoint);
+  res.send(data || []);
 });
 
-app.listen(3001, ()=> {
-    console.log('Listening on port 3001')
+app.listen(3001, () => {
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
 import './style/styles.css';
-
-console.log('it is working');
