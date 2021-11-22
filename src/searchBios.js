2af1c60ccd459786307ec2c941b2c72124ff49d4
menyagah <menyagah27@gmail.com>
2021-11-22 10:39:12 +0300

    Refactor data rendering logic

--- src/searchBios.js
+++ src/searchBios.js
@@ -1,22 +1,43 @@
-import debounce from './debounce.js';
-
+/* eslint-disable no-loop-func */
 const { default: axios } = require('axios');
 
-const fetchData = async (searchTerm) => {
-  // const response = await axios.get(`http://localhost:3001/people?username=${searchTerm}`); people route
-  const response = await axios.get(`http://localhost:3001/username?username=${searchTerm}`);
-  if (response.data.Error) {
-    console.log([]);
+const btnBio = document.getElementById('bio');
+const btnPeople = document.getElementById('people');
+const btnJob = document.getElementById('job');
+const btnSearch = document.getElementById('button-addon2');
+const inputSearch = document.querySelector('.form-control');
+
+const btns = [btnBio, btnPeople, btnJob];
+
+let buttonClicked = 'job';
+
+for (let i = 0; i < btns.length; i += 1) {
+  btns[i].addEventListener('click', () => {
+    buttonClicked = btns[i].id;
+    console.log(`you clicked: ${btns[i].id}`);
+  });
+}
+
+const trigger = btnSearch.addEventListener('click', () => {
+  if (buttonClicked === 'job') {
+    const fetchData = async (searchTerm) => {
+      const response = await axios.get(`http://localhost:3001/search?text=${searchTerm}`);
+      console.log(response.data);
+    };
+    fetchData(inputSearch.value);
+  } else if (buttonClicked === 'bio') {
+    const fetchData = async (searchTerm) => {
+      const response = await axios.get(`http://localhost:3001/username?username=${searchTerm}`);
+      console.log(response.data);
+    };
+    fetchData(inputSearch.value);
+  } else {
+    const fetchData = async (searchTerm) => {
+      const response = await axios.get(`http://localhost:3001/people?username=${searchTerm}`);
+      console.log(response.data);
+    };
+    fetchData(inputSearch.value);
   }
-  console.log(response.data);
-};
-
-const input = document.querySelector('input');
-
-const onInput = (event) => {
-  fetchData(event.target.value);
-};
-
-const trigger = input.addEventListener('input', debounce(onInput));
+});
 
 export default trigger;
